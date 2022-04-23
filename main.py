36d76425816241b216b82ac3a621b98db7d294dd
MarkSluis <mark.sluis@outlook.com>
2022-04-23 17:09:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class MySQL():
         self.user = input_user
         self.password = input_password
     def help(self):
-        print("Help")
+        print("https://github.com/MarkSluis/Personal_MySQL")
     def show_database(self):
         db = mysql.connector.connect(
             host = self.host,
@@ -81,7 +81,7 @@ class MySQL():
         cursor.execute("ALTER TABLE " + table + " ADD COLUMN " + string)
         for x in dict_column.keys():
             print(f"Column {x} is added to table {table} in database {database}")
-    def insert_data_in_table_in_database(self, database, table, dict_colum):
+    def insert_dctdata_in_list_in_table_in_database(self, database, table, dict_lst):
         db = mysql.connector.connect(
             host = self.host,
             user = self.user,
@@ -89,7 +89,35 @@ class MySQL():
             database = database
         )
         cursor = db.cursor()
+        for dct in dict_lst:
+            placeholder = ', '.join(['%s'] * len(dct))
+            columns = ', '.join(dct.keys())
+            sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (table, columns, placeholder)
+            cursor.execute(sql, list(dct.values()))
+        print(f"Data is added to {table} in database {database}")
+        db.commit()
 
+
+    def print_select(self, database, table, select_column = "*"):
+        db = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database = database
+        )
+        cursor = db.cursor()
+        if type(select_column) == str:
+            cursor.execute("SELECT " + select_column + " FROM " + table)
+        elif type(select_column) == list:
+            temp_str = ""
+            for i in select_column:
+                temp_str += i + ", "
+            cursor.execute("SELECT " + temp_str[:-2] + " FROM " + table)
+        result = cursor.fetchall()
+        print("how the query is executed and returned: ")
+        for i in result:
+            print(i)
+        return result
 
 # Test dropping and creating schemes
 db = MySQL("localhost", "bitacademy", "bitacademy")
@@ -109,9 +137,14 @@ db.show_tables_in_database("academy")
 
 # Test insert data in table
 lst_data = [{"first_name": "Mark", "last_name": "Sluis"}, {"first_name": "Marit", "last_name": "de Boer"}, {"first_name": "Femke", "last_name": "de Boer"}, {"first_name": "Pip", "last_name": "Sluis"}, {"first_name": "Dwarrel", "last_name": "Lego"}]
+db.insert_dctdata_in_list_in_table_in_database("academy", "persons", lst_data)
 
-# Delete everything
-db.drop_database("academy")
+# Test select data
+db.print_select("academy", "persons", ["first_name", "last_name"])
+db.print_select("academy", "persons", "first_name")
+db.print_select("academy", "persons")
+
+# Help
 db.help()
 
 
